d3507ecf14ffed4b84b197e9082aab5418d65042
Ofir Gur <ofir.gur@browzwear.com>
2022-07-31 16:24:21 +0300

    update

--- src/components/AppLayout/components/Header/styled.tsx
+++ src/components/AppLayout/components/Header/styled.tsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const HeaderStyled = styled.h2``;
+export const HeaderStyled = styled.h2`
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #fff;
+`;
--- src/components/AppLayout/components/Pages/index.tsx
+++ src/components/AppLayout/components/Pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { PagesStyled } from './styled';
 
 const Pages = () => {
-  return <div>Pages</div>;
+  return <PagesStyled>Pages</PagesStyled>;
 };
 
 export default Pages;
