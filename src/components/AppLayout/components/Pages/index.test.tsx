cdd2cf0feb6e3c89cd04942934d814da789e1d83
Ofir Gur <ofir.gur@browzwear.com>
2022-08-01 09:49:42 +0300

    update

--- src/components/AppLayout/components/Pages/index.test.tsx
+++ src/components/AppLayout/components/Pages/index.test.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Pages from '.';
 
 describe('Pages', () => {
   it('should render correctly', () => {
-    const { container } = render(<Pages />);
+    const { container } = render(
+      <BrowserRouter>
+        <Pages />
+      </BrowserRouter>
+    );
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import LeftPanel from './components/LeftPanel';
 import Main from './components/Main';
 import { AppLayoutStyled } from '../AppLayout/styled';
 
 const AppLayout = () => {
   return (
-    <BrowserRouter>
-      <AppLayoutStyled>
-        <LeftPanel />
-        <Main />
-      </AppLayoutStyled>
-    </BrowserRouter>
+    <AppLayoutStyled>
+      <LeftPanel />
+      <Main />
+    </AppLayoutStyled>
   );
 };
 
