61fcc9e8b2c7dd92cd8629c2aeed2bec004b7753
Ofir Gur <ofir.gur@browzwear.com>
2022-08-01 09:51:53 +0300

    update

--- src/components/AppLayout/components/Pages/index.tsx
+++ src/components/AppLayout/components/Pages/index.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
 import { PagesStyled } from './styled';
 
-const Home = () => <div>Homeeeeeeeeeeeeeeeeeeeeeeeee</div>;
-const Teams = () => <div>Teams</div>;
-const Team = () => <div>Team</div>;
-const NewTeamForm = () => <div>NewTeamForm</div>;
-const LeagueStandings = () => <div>LeagueStandings</div>;
-
 const Pages = () => {
-  return (
-    <PagesStyled>
-      <Routes>
-        <Route path="/" element={<Outlet />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
-      </Routes>
-    </PagesStyled>
-  );
+  return <PagesStyled>PagesStyled</PagesStyled>;
 };
 
 export default Pages;
