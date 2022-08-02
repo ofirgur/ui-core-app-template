f536daa8af5bfda2a4e2045d70243a0692a1d469
Ofir Gur <ofir.gur@browzwear.com>
2022-08-02 15:41:07 +0300

    update

--- src/components/AppLayout/components/LeftPanel/index.tsx
+++ src/components/AppLayout/components/LeftPanel/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCurrentRoute } from 'router/hooks';
 import routes from 'router/routes';
 import { LeftPanelStyled, ListItemStyled } from './styled';
 
 const { dashboard, garments, collections } = routes;
 
 const LeftPanel = () => {
+  const currentRoute = useCurrentRoute();
   const navigate = useNavigate();
   const handleNavigate = (pathname: string) => () => {
     navigate(pathname);
@@ -13,13 +15,22 @@ const LeftPanel = () => {
 
   return (
     <LeftPanelStyled>
-      <ListItemStyled onClick={handleNavigate(dashboard.pathname)}>
+      <ListItemStyled
+        onClick={handleNavigate(dashboard.pathname)}
+        active={dashboard.pathname === currentRoute.pathname}
+      >
         {dashboard.title}
       </ListItemStyled>
-      <ListItemStyled onClick={handleNavigate(garments.pathname)}>
+      <ListItemStyled
+        onClick={handleNavigate(garments.pathname)}
+        active={garments.pathname === currentRoute.pathname}
+      >
         {garments.title}
       </ListItemStyled>
-      <ListItemStyled onClick={handleNavigate(collections.pathname)}>
+      <ListItemStyled
+        onClick={handleNavigate(collections.pathname)}
+        active={collections.pathname === currentRoute.pathname}
+      >
         {collections.title}
       </ListItemStyled>
     </LeftPanelStyled>
--- src/components/AppLayout/components/LeftPanel/styled.tsx
+++ src/components/AppLayout/components/LeftPanel/styled.tsx
@@ -6,13 +6,18 @@ export const LeftPanelStyled = styled.ul`
   margin-top: 70px;
 `;
 
-export const ListItemStyled = styled.li`
+type ListItemStyledType = {
+  active: boolean;
+};
+
+export const ListItemStyled = styled.li<ListItemStyledType>`
   display: flex;
   align-items: center;
   height: 50px;
   cursor: pointer;
   padding: 10px;
   font-size: 20px;
+  background-color: ${(p) => (p.active ? '#262626' : '')};
   &:hover {
     background-color: #262626;
   }
