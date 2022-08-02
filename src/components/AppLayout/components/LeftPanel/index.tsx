aa26c3e43637f0ada9879b0bd52b1fdcd31fc97c
Ofir Gur <ofir.gur@browzwear.com>
2022-08-02 15:29:03 +0300

    update

--- src/components/AppLayout/components/LeftPanel/index.tsx
+++ src/components/AppLayout/components/LeftPanel/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import { LeftPanelStyled } from './styled';
+import { useNavigate } from 'react-router-dom';
+import routes from 'router/routes';
+import { LeftPanelStyled, ListItemStyled } from './styled';
+
+const { dashboard, garments, collections } = routes;
 
 const LeftPanel = () => {
-  return <LeftPanelStyled>Left Panel</LeftPanelStyled>;
+  const navigate = useNavigate();
+  const handleNavigate = (pathname: string) => () => {
+    navigate(pathname);
+  };
+
+  return (
+    <LeftPanelStyled>
+      <ListItemStyled onClick={handleNavigate(dashboard.pathname)}>
+        {dashboard.title}
+      </ListItemStyled>
+      <ListItemStyled onClick={handleNavigate(garments.pathname)}>
+        {garments.title}
+      </ListItemStyled>
+      <ListItemStyled onClick={handleNavigate(collections.pathname)}>
+        {collections.title}
+      </ListItemStyled>
+    </LeftPanelStyled>
+  );
 };
 
 export default LeftPanel;
--- src/components/AppLayout/components/LeftPanel/styled.tsx
+++ src/components/AppLayout/components/LeftPanel/styled.tsx
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
 import variables from 'utils/variables';
 
-export const LeftPanelStyled = styled.div`
+export const LeftPanelStyled = styled.ul`
   width: ${variables.layout.leftPanel.width}px;
+  margin-top: 70px;
+`;
+
+export const ListItemStyled = styled.li`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  cursor: pointer;
+  padding: 10px;
+  font-size: 20px;
+  &:hover {
+    background-color: #262626;
+  }
 `;
