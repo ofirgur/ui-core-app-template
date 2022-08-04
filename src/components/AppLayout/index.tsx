263a72bc7929641c14bea9210abc604bee9768e0
Ofir Gur <ofir.gur@browzwear.com>
2022-08-04 12:43:10 +0300

    update

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 import LeftPanel from './components/LeftPanel';
 import Main from './components/Main';
 import { AppLayoutStyled } from '../AppLayout/styled';
 
 const AppLayout = () => {
+  const theme = useTheme();
+  console.log('theme: ', theme);
   return (
     <AppLayoutStyled>
       <LeftPanel />
--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -1,3 +1,21 @@
-export default {
-  main: "mediumseagreen"
-};
+import { createTheme } from '@material-ui/core/styles';
+
+declare module '@material-ui/core/styles/createTheme' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+export default createTheme({
+  status: {
+    danger: 'orange',
+  },
+});
