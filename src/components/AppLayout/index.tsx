760ec30e1f7134e8be8adfcbf8cac94c974e6c1d
Ofir Gur <ofir.gur@browzwear.com>
2022-08-14 08:56:44 +0300

    update

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useTheme } from 'styled-components';
+//import { useTheme } from 'styled-components';
 import LeftPanel from './components/LeftPanel';
 import Main from './components/Main';
 import { AppLayoutStyled } from '../AppLayout/styled';
 
 const AppLayout = () => {
-  const theme = useTheme();
-  console.log('theme: ', theme);
+  //const theme = useTheme();
+  //console.log('theme: ', theme);
   return (
     <AppLayoutStyled>
       <LeftPanel />
