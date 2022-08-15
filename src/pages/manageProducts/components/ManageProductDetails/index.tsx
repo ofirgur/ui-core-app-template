77053e27fdec14addb03edf1df088953131646d2
Ofir Gur <ofir.gur@browzwear.com>
2022-08-15 16:14:36 +0300

    update

--- src/pages/manageProducts/components/ManageProductDetails/index.tsx
+++ src/pages/manageProducts/components/ManageProductDetails/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Product } from 'pages/manageProducts/types';
+import { useManageProductsContext } from 'pages/manageProducts/state/context';
+import { StyledProductTitle } from './styled';
 
 const ManageProductDetails = () => {
   const { productId } = useParams();
+  const { manageProductsState } = useManageProductsContext();
+  const { products } = manageProductsState;
+  const product = products.find((p: Product) => p.id === productId);
 
-  return <div>Product ID: {productId}</div>;
+  return (
+    <>
+      <StyledProductTitle>Product - {product.name}</StyledProductTitle>
+    </>
+  );
 };
 
 export default ManageProductDetails;
