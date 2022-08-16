18ff3132224b993acf81df7547c40c72ce14ae2f
Ofir Gur <ofir.gur@browzwear.com>
2022-08-16 10:46:08 +0300

    update

--- src/pages/template/index.tsx
+++ src/pages/template/index.tsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import { MANAGE_PRODUCTS_INITIAL_STATE } from 'pages/manageProducts/utils/state/initial';
+import { ManageProductsContextProvider } from './utils/state/context';
 import ManageProductsOutlet from './components/ManageProductsOutlet';
-export default ManageProductsOutlet;
+
+const ManageProducts = () => {
+  return (
+    <ManageProductsContextProvider initialState={MANAGE_PRODUCTS_INITIAL_STATE}>
+      <ManageProductsOutlet />
+    </ManageProductsContextProvider>
+  );
+};
+
+export default ManageProducts;
